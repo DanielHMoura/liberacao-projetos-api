401426abbe8700262690b1d1e0fc59aa215859bc
DanielHMoura <danielsos17@gmail.com>
2026-01-16 19:20:53 -0300

    Gerador de QRCODE

--- src/main/java/com/metrica/liberacao/controller/PixController.java
+++ src/main/java/com/metrica/liberacao/controller/PixController.java
@@ -4,6 +4,8 @@ import com.metrica.liberacao.service.QrCodeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/pix")
@@ -13,13 +15,20 @@ public class PixController {
     private QrCodeService qrCodeService;
 
     @GetMapping("/gerar-qrcode")
-    public ResponseEntity<String> gerarQrCode(
+    public ResponseEntity<Map<String, String>> gerarQrCode(
             @RequestParam String chavePix,
             @RequestParam String nomeBeneficiario,
             @RequestParam String cidade,
             @RequestParam(required = false) Double valor) {
 
         String qrCode = qrCodeService.gerarQrCodeBase64(chavePix, nomeBeneficiario, cidade, valor);
-        return ResponseEntity.ok(qrCode);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("qrCodeImage", qrCode);
+        response.put("chavePix", chavePix);
+        response.put("nomeBeneficiario", nomeBeneficiario);
+        response.put("cidade", cidade);
+
+        return ResponseEntity.ok(response);
     }
 }
