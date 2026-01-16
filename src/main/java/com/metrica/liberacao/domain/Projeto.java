ab4b7bd8786180590dcf44c3918eceea091c3050
DanielHMoura <danielsos17@gmail.com>
2026-01-16 04:18:59 -0300

    fix: remover campos de autenticação da entidade Projeto

--- src/main/java/com/metrica/liberacao/domain/Projeto.java
+++ src/main/java/com/metrica/liberacao/domain/Projeto.java
@@ -23,14 +23,6 @@ public class Projeto {
     @Column(nullable = false)
     private String nomeCliente;
 
-    @Column(unique = true, nullable = false)
-    private String username;
-
-    @Column(nullable = false)
-    private String password;
-
-    @Column(nullable = false)
-    private String role;
 
     @Enumerated(EnumType.STRING)
     private StatusAnteprojeto statusAnteprojeto;
@@ -77,12 +69,4 @@ public class Projeto {
     public byte[] getPdfExecutivo() {return pdfExecutivo;}
     public void setPdfExecutivo(byte[] pdfExecutivo) {this.pdfExecutivo = pdfExecutivo;}
 
-    public String getUsername() {return username;}
-    public void setUsername(String username) {this.username = username;}
-
-    public String getPassword() {return password;}
-    public void setPassword(String password) {this.password = password;}
-
-    public String getRole() {return role;}
-    public void setRole(String role) {this.role = role;}
 }
