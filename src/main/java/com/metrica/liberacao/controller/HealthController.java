cb67dc46576f4b1adadc75a11bad2c14da04bcb8
DanielHMoura <danielsos17@gmail.com>
2026-01-30 10:24:14 -0300

    Configuracao do Health

--- src/main/java/com/metrica/liberacao/controller/HealthController.java
+++ src/main/java/com/metrica/liberacao/controller/HealthController.java
@@ -1,13 +1,22 @@
 package com.metrica.liberacao.controller;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    public HealthController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @GetMapping("/health")
     public String health() {
-        return "API funcionando!";
+        jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+        return "API e banco funcionando!";
     }
 }
+
