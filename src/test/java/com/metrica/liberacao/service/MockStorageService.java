4b247f6756dbe07985d840b19fe0f2405c515b51
DanielHMoura <danielsos17@gmail.com>
2026-02-03 16:07:30 -0300

    Correcao da rota UPTIMEROBOT

--- src/test/java/com/metrica/liberacao/service/MockStorageService.java
+++ src/test/java/com/metrica/liberacao/service/MockStorageService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service("supabaseStorageService")
-@Profile("test")
+@Profile("!prod")
 public class MockStorageService implements StorageService {
 
     @Override
